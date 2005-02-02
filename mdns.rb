7576bac1cf29a0f1f74e96ad647b6d7740b81db4
Sam Roberts <vieuxtech@gmail.com>
2005-02-02 03:47:26 +0000

    cleanup, simpler, more obscure, more automated, etc.

--- mdns.rb
+++ mdns.rb
@@ -1,6 +1,8 @@
-#!/usr/local/bin/ruby18 -wI../ruby/lib
+#!/opt/local/bin/ruby -w
 
-$: << File.dirname($0)
+$:.unshift(File.dirname($0))
+
+puts $:
 
 require 'getoptlong'
 require 'net/dns/mdns'
@@ -32,25 +34,19 @@ Examples:
 EOF
 
 opt_debug = nil
-opt_svc = nil
+opt_type = Resolv::DNS::Resource::IN::ANY
 
 opts = GetoptLong.new(
   [ "--help",    "-h",              GetoptLong::NO_ARGUMENT ],
-  [ "--svc",     "-s",              GetoptLong::NO_ARGUMENT ],
+  [ "--type",    "-t",              GetoptLong::REQUIRED_ARGUMENT],
   [ "--debug",   "-d",              GetoptLong::NO_ARGUMENT ]
 )
 
 opts.each do |opt, arg|
   case opt
-    when "--help" then
-      puts HELP
-      exit 0
-
-    when "--debug" then
-      opt_debug = true
-
-    when "--svc"
-      opt_svc = true
+    when "--help"  then puts HELP; exit 0
+    when "--debug" then opt_debug = true
+    when "--type"  then opt_type = rrmap[arg]
   end
 end
 
@@ -58,55 +54,42 @@ r = Resolv::MDNS.new
 
 r.lazy_initialize
 
-if opt_debug
-  pp r
-end
-
 Name = Resolv::DNS::Name
 
-argv0 = Name.create(ARGV[0])
+ARGV.each do |n|
+  argv0 = Name.create(n)
 
-unless argv0.absolute?
-  if argv0.to_s[0] == ?_
-    if argv0.length == 1
-      argv0 = Name.create(argv0.to_s + '._tcp')
-    end
+  unless argv0.absolute?
+    if argv0.to_s[0] == ?_
+      if argv0.length == 1
+        argv0 = Name.create(argv0.to_s + '._tcp')
+      end
 
-    if argv0.length == 2
-      argv0 = Name.create(argv0.to_s + '.local')
-    end
-  else
-    if argv0.length == 1
-      argv0 = Name.create(argv0.to_s + '.local')
+      if argv0.length == 2
+        argv0 = Name.create(argv0.to_s + '.local')
+      end
+    else
+      if argv0.length == 1
+        argv0 = Name.create(argv0.to_s + '.local')
+      end
     end
   end
-end
 
-rrs = r.getresources(argv0, rrmap[ARGV[1]])
+  puts "#{n} -> #{argv0}"
 
-if !opt_svc
-  pp rrs
-  exit 0
-end
+# r.each_resource(argv0, opt_type) do |rr| # BUG - this never times out...
+  r.getresources(argv0, opt_type).each do |rr|
+    case rr
+    when Resolv::DNS::Resource::IN::PTR
+      n = rr.name
 
+      puts "type=#{argv0} instance=#{n}"
 
-rrs.each do |rr|
-  if(Resolv::DNS::Resource::IN::PTR === rr)
-    n = rr.name
-
-    puts "type=#{ARGV[0]} instance=#{n}"
-
-    r.each_resource(n, Resolv::DNS::Resource::IN::ANY) do |svcrr|
-      case svcrr
-      when Resolv::DNS::Resource::IN::SRV
-        puts('  ' + svcrr.inspect)
-
-      when Resolv::DNS::Resource::IN::TXT
-        puts('  IN::TXT ' + svcrr.data)
-      else
-        # unexpected!
-        pp svcrr
+      r.each_resource(n, Resolv::DNS::Resource::IN::ANY) do |rr1|
+        pp rr1
       end
+    else
+      pp rr
     end
   end
 end
