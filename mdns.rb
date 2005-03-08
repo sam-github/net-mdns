a41415388034058fb9f504bbf9a815767023925b
Sam Roberts <vieuxtech@gmail.com>
2005-03-08 04:54:40 +0000

    options to use native/pure-ruby DNSSD, not working, always uses mine...

--- mdns.rb
+++ mdns.rb
@@ -4,21 +4,6 @@ $:.unshift(File.dirname($0))
 
 require 'getoptlong'
 
-require 'net/dns/mdns-sd.rb'
-
-# TODO - see if I can use DNSSD from require 'dnssd'
-DNSSD = Net::DNS::DNSSD
-
-rrmap = {
-  'a'   => Resolv::DNS::Resource::IN::A,
-  'any' => Resolv::DNS::Resource::IN::ANY,
-  'ptr' => Resolv::DNS::Resource::IN::PTR,
-  'srv' => Resolv::DNS::Resource::IN::SRV,
-  nil   => Resolv::DNS::Resource::IN::ANY
-}
-
-rtypes = rrmap.keys.join ', '
-
 =begin
 Apple's dns-sd options:
 
@@ -45,9 +30,9 @@ Apple's dns-sd options:
 
 HELP =<<EOF
 Usage: 
-  mdns -B        <Type> [Domain]         (Browse for service instances)
-  mdns -L <Name> <Type> [Domain]           (Look up a service instance)
-  mdns -R <Name> <Type> [Domain] <Port> [<TXT>...] (Register a service)
+  mdns [options] -B        <Type> [Domain]         (Browse for service instances)
+  mdns [options] -L <Name> <Type> [Domain]           (Look up a service instance)
+  mdns [options] -R <Name> <Type> [Domain] <Port> [<TXT>...] (Register a service)
 
 [Domain] is optional for -B, -L, and -R, it defaults to "local".
 
@@ -55,6 +40,12 @@ Usage:
 
 You can use long names --browse, --lookup, and --register instead of -B, -L,
 and -R.
+
+Options:
+  --native   Attempt to use 'dnssd', the interface to the native DNS-SD
+             resolver library.
+  --ruby     Attempt to use 'net/dns/mdns-sd', a pure-ruby DNS-SD resolver
+             library.
 
 Examples:
   mdns -B _daap._tcp
@@ -65,6 +56,8 @@ EOF
 opts = GetoptLong.new(
   [ "--debug",    "-d",               GetoptLong::NO_ARGUMENT ],
   [ "--help",     "-h",               GetoptLong::NO_ARGUMENT ],
+  [ "--native",   "-n",               GetoptLong::NO_ARGUMENT ],
+  [ "--ruby",     "-r",               GetoptLong::NO_ARGUMENT ],
 
   [ "--browse",    "-B",              GetoptLong::NO_ARGUMENT ],
   [ "--lookup",    "-L",              GetoptLong::NO_ARGUMENT ],
@@ -80,6 +73,9 @@ opts.each do |opt, arg|
   when "--help"
     print HELP
     exit 0
+
+  when '--dnssd'
+    require 'dnssd'
 
   when "--browse"
     @cmd = :browse
@@ -111,15 +107,24 @@ opts.each do |opt, arg|
   end
 end
 
+begin
+  DNSSD.class
+  puts "Using native DNSSD..."
+rescue NameError
+  require 'net/dns/mdns-sd.rb'
+  DNSSD = Net::DNS::DNSSD
+  puts "Using net::dns::DNSSD..."
+end
+
 unless @cmd
   print HELP
   exit 1
 end
 
-pp( @cmd, @type, @domain )  if @debug
-
 case @cmd
 when :browse
+  pp( @cmd, @type, @domain )  if @debug
+
   fmt = "%-3.3s  %-10.10s   %-15.15s  %-20.20s\n"
   printf fmt, "Ifx", "Domain", "Service Type", "Instance Name"
 
@@ -132,6 +137,8 @@ when :browse
 
 
 when :lookup
+  pp( @cmd, @name, @type, @domain )  if @debug
+
   fmt = "%-3.3s  %-10.10s   %-15.15s  %-20.20s %-20.20s %s\n"
   printf fmt, "Ifx", "Domain", "Service Type", "Instance Name", "Location", "Text"
 
@@ -145,6 +152,8 @@ when :lookup
   handle.stop
 
 when :register
+  pp( @cmd, @name, @type, @domain, @port, @txt)  if @debug
+
   fmt = "%-3.3s  %-10.10s   %-15.15s  %-20.20s %-20.20s %s\n"
   printf fmt, "Ifx", "Domain", "Service Type", "Instance Name", "Location", "Text"
 
