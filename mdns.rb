4acf48538a06bc532b1c15876e931d5459620e25
Sam Roberts <vieuxtech@gmail.com>
2005-03-20 16:28:35 +0000

    works with DNSSD now

--- mdns.rb
+++ mdns.rb
@@ -106,6 +106,7 @@ opts.each do |opt, arg|
 
   when '--dnssd'
     require 'dnssd'
+    require 'socket'
 
   when "--browse"
     @cmd = :browse
@@ -145,11 +146,27 @@ end
 begin
   DNSSD.class
   puts "Using native DNSSD..."
+  module DNSSD
+    def self.namesplit(n)
+      n.scan(/(?:\\.|[^\.])+/)
+    end
+    class ResolveReply
+      def domain
+        DNSSD.namesplit(fullname)[-1]
+      end
+      def type
+        DNSSD.namesplit(fullname)[1,2].join('.')
+      end
+      def name
+        DNSSD.namesplit(fullname)[0]
+      end
+    end
+  end
 rescue NameError
   require 'net/dns/mdns-sd'
   DNSSD = Net::DNS::MDNSSD
   Net::DNS::MDNS::Responder.instance.log = @log if @log
-  puts "Using net::dns::MDNSSD..."
+  puts "Using Net::DNS::MDNSSD..."
 end
 
 unless @cmd
@@ -159,13 +176,17 @@ end
 
 case @cmd
 when :browse
-  STDERR.puts( "#{@cmd}(#{@type}, #{@domain}) =>" )  if @debug
+  STDERR.puts( "DNSSD.#{@cmd}(#{@type}, #{@domain}) =>" )  if @debug
 
   fmt = "%-3.3s  %-8.8s   %-15.15s  %-20.20s\n"
   printf fmt, "Ttl", "Domain", "Service Type", "Instance Name"
 
   handle = DNSSD.browse(@type, @domain) do |reply|
-    printf fmt, reply.flags.to_i, reply.domain, reply.type, reply.name
+    begin
+      printf fmt, reply.flags.to_i, reply.domain, reply.type, reply.name
+    rescue
+      p $!
+    end
   end
 
   $stdin.gets
@@ -173,30 +194,38 @@ when :browse
 
 
 when :lookup
-  STDERR.puts( "#{@cmd}(#{@name}, #{@type}, #{@domain}) =>" )  if @debug
+  STDERR.puts( "DNSSD.#{@cmd}(#{@name}, #{@type}, #{@domain}) =>" )  if @debug
 
   fmt = "%-3.3s  %-8.8s   %-19.19s  %-20.20s %-20.20s %s\n"
   printf fmt, "Ttl", "Domain", "Service Type", "Instance Name", "Location", "Text"
 
   handle = DNSSD.resolve(@name, @type, @domain) do |reply|
-    location = "#{reply.target}:#{reply.port}"
-    text = reply.text_record.to_a.map { |kv| "#{kv[0]}=#{kv[1].inspect}" }.join(', ')
-    printf fmt, reply.flags.to_i, reply.domain, reply.type, reply.name, location, text
+    begin
+      location = "#{reply.target}:#{reply.port}"
+      text = reply.text_record.to_a.map { |kv| "#{kv[0]}=#{kv[1].inspect}" }.join(', ')
+      printf fmt, reply.flags.to_i, reply.domain, reply.type, reply.name, location, text
+    rescue
+      p $!
+    end
   end
 
   $stdin.gets
   handle.stop
 
 when :register
-  pp( @cmd, @name, @type, @domain, @port, @txt)  if @debug
+  STDERR.puts( "DNSSD.#{@cmd}(#{@name}, #{@type}, #{@domain}, #{@port}, #{@txt.inspect}) =>" )  if @debug
 
   fmt = "%-3.3s  %-8.8s   %-19.19s  %-20.20s %-20.20s %s\n"
   printf fmt, "Ttl", "Domain", "Service Type", "Instance Name", "Location", "Text"
 
   handle = DNSSD.register(@name, @type, @domain, @port, @txt) do |notice|
-    location = "#{Socket.gethostname}:#{@port}"
-    text = @txt.to_a.map { |kv| "#{kv[0]}=#{kv[1].inspect}" }.join(', ')
-    printf fmt, 'N/A', notice.domain, notice.type, notice.name, location, text
+    begin
+      location = "#{Socket.gethostname}:#{@port}"
+      text = @txt.to_a.map { |kv| "#{kv[0]}=#{kv[1].inspect}" }.join(', ')
+      printf fmt, 'N/A', notice.domain, notice.type, notice.name, location, text
+    rescue
+      p $!
+    end
   end
 
   $stdin.gets
